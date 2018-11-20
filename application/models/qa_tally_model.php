20bcfae501231fd0834162bc21b2047b0a4556b4
John Geliberte <gelibertejohn@gmail.com>
2018-11-20 17:55:45 +0800

    added extra validation for qa tally page

--- application/models/qa_tally_model.php
+++ application/models/qa_tally_model.php
@@ -24,7 +24,7 @@
 		}
 
 		public function getRecipientsForSite($site_id) {
-			$query = "SELECT DISTINCT users.user_id, users.firstname, users.lastname, user_organization.org_name, sites.site_code, user_mobile.sim_num, sites.purok, sites.sitio, sites.barangay, sites.municipality, sites.province FROM comms_db.users INNER JOIN user_organization ON users.user_id = user_organization.user_id INNER JOIN sites ON user_organization.fk_site_id = sites.site_id INNER JOIN user_mobile ON users.user_id = user_mobile.user_id INNER JOIN user_ewi_status ON users.user_id = user_ewi_status.users_id WHERE sites.site_id = '".$site_id."' AND user_ewi_status.status = '1';";
+			$query = "SELECT DISTINCT users.user_id, users.firstname, users.lastname, user_organization.org_name, sites.site_code, user_mobile.sim_num, sites.purok, sites.sitio, sites.barangay, sites.municipality, sites.province FROM comms_db.users INNER JOIN user_organization ON users.user_id = user_organization.user_id INNER JOIN sites ON user_organization.fk_site_id = sites.site_id INNER JOIN user_mobile ON users.user_id = user_mobile.user_id INNER JOIN user_ewi_status ON users.user_id = user_ewi_status.users_id WHERE sites.site_id = '".$site_id."' AND user_ewi_status.status = '1' AND users.status = '1' AND user_mobile.mobile_status = '1'";
 			$result = $this->db->query($query);
 			if ($result->num_rows != 0) {
 				$recipients = $result->result();
@@ -35,7 +35,7 @@
 		}
 
 		public function insertSettingsForTally($table_source, $tally) {
-			$query = "INSERT INTO ".$table_source." VALUES (0,'".$tally['event_id']."','".$tally['ewi_expected']."','".$tally['ewi_actual']."','".$tally['status']."','".$tally['ts']."')";
+			$query = "INSERT INTO ".$table_source." VALUES (0,'".$tally['event_id']."','".$tally['ewi_expected']."','".$tally['ewi_actual']."','".$tally['status']."','".$tally['ts']."',".$tally['site_id'].")";
 			$result = $this->db->query($query);
 			return $result;
 		}
